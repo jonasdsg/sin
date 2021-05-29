48517557da2e63769e7c24a8266e59dee2dcd89d
Jonas Góes <jonasdossantos1991@gmail.com>
2021-05-28 21:05:34 -0300

    criada função de atribuição de tema por pesquisa

---
+++ js/data.js
@@ -0,0 +1,37 @@
+export const eticaMessage = `O prejuízo causado por ataques virtuais pode custar muito para uma
+empresa, às vezes podendo até levá-la a falir. Com objetivo de contornar
+isso, cada vez mais empresas estão investindo em segurança cibernética.
+Grandes nomes como a Google, por exemplo, pagam profissionais para
+achar brechas na segurança de seus sistemas e produzirem soluções para
+essas vulnerabilidades. O termo para esse conceito é o da “Ética Hacker”.
+Também conhecido como “pentester” (em tradução livre, alguém que
+testa possíveis penetrações em um sistema), o hacker ético é o profissional
+especializado em “Offensive Security” (Segurança Ofensiva), onde ele busca
+vulnerabilidades que poderiam ser exploradas por hackers que visam lucrar
+com golpes e ataques virtuais, tentando assim ficar um passo à frente, por
+assim dizer, de possíveis ameaças. Após detectar uma falha, o hacker ético
+trabalha em consertar esse erro, mantendo assim a segurança da empresa
+para a qual ele trabalha. ÉTICA HACKER`.replace(/(?!\w|\s)/,"");
+
+export const mercadoMessage = `Muitas empresas estão contratando profissionais de segurança
+cibernética. É uma das áreas que mais crescem no mercado, sendo uma das
+principais tendências em tecnologia nos próximos anos.
+Esta alta demanda de profissionais da área, deve-se à popularização e
+democratização da internet. Cada vez mais pessoas e empresas estão
+conectadas, e, com isso, mais dados são gerados. Consequentemente, os
+ciberataques estão ficando cada vez mais frequentes e complexos.
+O cenário atual do mercado de trabalho, devido a pandemia causada pelo
+novo coronavírus e com o crescimento do home office, proporciona mais
+vulnerabilidade e oferece um terreno fértil para ações criminosas e
+ciberataques, que vão de informações confidenciais até a paralisação de linhas
+de produção na indústria.
+O mercado está ainda mais em expansão, principalmente após a LEI DE
+PROTEÇÃO DE DADOS entrar em vigor. Essa lei determina que as empresas são
+obrigadas a criar mecanismos para zelar com a segurança das informações
+pessoais dos clientes. Em caso de vazamento, a empresa é multada.
+Por isso, a Segurança da Informação torna-se uma área ainda mais
+estratégica para os mais variados negócios, que agora precisam garantir a
+defesa da operação e das informações disponíveis.
+Com o aumento dessa demanda, as empresas precisam de mais
+especialistas em cibersegurança. A área pode ser uma ótima opção de carreira
+para quem se interessa ou já atua com tecnologia. Mercado de trabalho em segurança cibernética`.replace(/(?!\w|\s)/,"");;
--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+import { eticaMessage, mercadoMessage } from "./data.js";
 import { Card } from "./models/card.js";
 
 const conteudo = [
     {
         videoUrl:'https://www.youtube.com/embed/OpT0MMYoAk0',
         title:'ÉTICA HACKER',
-        message:'O prejuízo causado por ataques virtuais pode custar muito para uma empresa, às vezes podendo até levá-la a falir. Com objetivo de contornar isso, cada vez mais empresas estão investindo em segurança cibernética. Grandes nomes como a Google, por exemplo, pagam profissionais para achar brechas na segurança de seus sistemas e produzirem soluções para essas vulnerabilidades. O termo para esse conceito é o da “Ética Hacker”.',
+        message: eticaMessage,
         linkDoc:'https://drive.google.com/file/d/1MUsTrFhbB1jPpMea0SrO47Da5OSnx3Sn/view?usp=sharing'
     },
     {
@@ -22,7 +23,7 @@ const conteudo = [
     {
         videoUrl:'https://www.youtube.com/embed/OSWGHhjSFbs?start=33',
         title:'Mercado de trabalho',
-        message:'Muitas empresas estão contratando profissionais de segurança cibernética. É uma das áreas que mais crescem no mercado, sendo uma das principais tendências em tecnologia nos próximos anos.Esta alta demanda de profissionais da área, deve-se à popularização e democratização da internet. Cada vez mais pessoas e empresas estão conectadas, e, com isso, mais dados são gerados. Consequentemente, os ciberataques estão ficando cada vez mais frequentes e complexos.',
+        message: mercadoMessage,
         linkDoc:'https://drive.google.com/file/d/1Lh-OciuUTc3xmY3ewPZQgyD4qhMTREDS/view?usp=sharing'
     },
     {
@@ -35,6 +36,8 @@ const conteudo = [
 
 let loaded = false;
 
+let heavy = [0,0,0,0,0];
+let find = document.querySelector("#find");
 let b = document.querySelector("#main div div");
 let c = document.querySelector("#textContent");
 let d = document.querySelector("#description");
@@ -54,16 +57,7 @@ f.addEventListener('click',(e)=>{
             break;
         }
     }
-    if(!loaded){
-        loaded = true;
-        b.classList = "col-md-12 col-lg-8 ratio ratio-16x9 mb-2";
-        c.classList = "col-md-12 col-lg-8 mb-2";
-    }
-    let a = new Card(conteudo[index].videoUrl,conteudo[index].title,conteudo[index].message,conteudo[index].linkDoc);
-    c.removeChild(c.firstChild);
-    c.appendChild(a.textContent);
-    b.removeChild(b.firstChild);
-    b.appendChild(a.card)
+    setConteudo(index);
 })
 
 d.addEventListener('click',(e)=>{
@@ -72,3 +66,60 @@ d.addEventListener('click',(e)=>{
     }
 })
 
+find.addEventListener('keyup',(e)=>{
+    heavy = [0,0,0,0,0];
+    console.log(find.value);
+    let i = 0;
+ 
+    conteudo.forEach(cont=>{
+        let contentArray = null;
+        let keySplit = null;
+        
+        if(!keySplit){
+            keySplit = find?.value?.split(/\s/);
+        } 
+        if(!contentArray){
+            contentArray = cont?.message?.toLowerCase()?.split(/\s/);
+            console.log(contentArray)
+        }
+
+        if(keySplit?.length>1){
+
+            keySplit.forEach(m=>{
+                if(contentArray?.includes(m)){
+                    heavy[i]++
+                }
+            })
+        }
+
+        i++;
+    })
+    let maior = 0, index;
+    i = 0;
+    heavy.forEach(n=>{
+        if(n>maior){
+            index = i;
+            maior = n;
+        }
+        i++;
+    })
+
+    setConteudo(index);
+    console.log(index)
+    console.log(heavy);
+});
+
+function setConteudo(index){
+    if(conteudo[index]?.title){
+        if(!loaded){
+            loaded = true;
+            b.classList = "col-md-12 col-lg-8 ratio ratio-16x9 mb-2";
+            c.classList = "col-md-12 col-lg-8 mb-2";
+        }
+        let a = new Card(conteudo[index].videoUrl,conteudo[index].title,conteudo[index].message,conteudo[index].linkDoc);
+        c.removeChild(c.firstChild);
+        c.appendChild(a.textContent);
+        b.removeChild(b.firstChild);
+        b.appendChild(a.card)
+    }
+}
